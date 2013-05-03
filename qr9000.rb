3c59186ae9a8b1d513fa36f78b404946fc91b49a
Jim DeVona <jim.devona@gmail.com>
2013-05-03 19:00:32 -0400

    compartmentalized twitter error catchers

--- qr9000.rb
+++ qr9000.rb
@@ -7,62 +7,64 @@ require 'yaml'
 
 config = YAML.load_file('qr9000.yml')
 
-twitter = Twitter::Client.new(config[:authentication])
-
 begin
-	twitter.mentions(:since_id => config[:mostRecentMentionId], :include_entities => true).each do |mention|
-	
-		# This is now the most recent mention inspected - don't try to reply again
-		if mention.id > config[:mostRecentMentionId] then config[:mostRecentMentionId] = mention.id end
-		
-		# Ignore mentions that don't match expected format…
-		if !mention.text.match(/^(@QR9000\s+)(.+)$/i) then next end
-
-		# …but extract the content of any matches ($~ is the last MatchData)
-		offset = $~.captures[0].length
-		content = $~.captures[1]
-		
-		# newtext is a copy of content, but with any t.co URLs expanded to original.
-		laststart = 0
-		newtext = ''
-		mention.urls.each do |url|
-			# if there is no text before the URL, this will fail with index - offset = 0 & 0 - 1 = -1,
-			# inserting the whole content string ([0..-1]) instead of nothing.
-			if url.indices[0]-1 > offset then newtext += content[laststart..(url.indices[0]-1-offset)] end
-			newtext += url.expanded_url
-			laststart = url.indices[1]-offset
-		end
-		newtext += content[laststart..-1]
-		
-		# (consider logging id/text/timestamp)
-		
-		size = 4
-		begin
-			qr = RQRCode::QRCode.new(newtext, :size => size)
-		rescue RQRCode::QRCodeRunTimeError => qerr
-	
-			# Content too big for size. Let error stand if already largest size.
-			if size == 40 then
-				raise qerr
-			end
-			
-			# Otherwise, increase the size and try again.
-			size += 1
-			retry
-			
-		end
-	
-		# Generate a PNG image of the QR code
-		png = qr.to_img.resize(480, 480)
-		
-		# Post the QR code image in reply; rgb format explicit (default grayscale PNG appears to confuse Twitter)
-		#reply = twitter.update_with_media(format("@%s ", mention.user.screen_name), png.to_datastream(:fast_rgb), :in_reply_to_status_id => mention.id)
-		# Also, validate reply response.
-		
-	end
-rescue Twitter::Error => e
-	puts STDERR, e
+	twitter = Twitter::Client.new(config[:authentication])
+	mentions = twitter.mentions(:since_id => config[:mostRecentMentionId], :include_entities => true)
+rescue Twitter::Error => err
+	puts STDERR, err
+	exit
 end
 
-# Update config file with mostRecentMentionId
-#File.open('qr9000.yml', 'w') {|file| YAML.dump(config, file)}
+mentions.each do |mention|
+	
+	# This is now the most recent mention inspected - don't try to reply again
+	if mention.id > config[:mostRecentMentionId] then config[:mostRecentMentionId] = mention.id end
+	
+	# Ignore mentions that don't match expected format…
+	if !mention.text.match(/^(@QR9000\s+)(.+)$/i) then next end
+
+	# …but extract the content of any matches ($~ is the last MatchData)
+	offset = $~.captures[0].length
+	tweet = $~.captures[1]
+	
+	# content is a copy of tweet, but with t.co URLs expanded to their original form.
+	laststart = 0
+	content = ''
+	mention.urls.each do |url|
+		if url.indices[0]-1 > offset then content += tweet[laststart..(url.indices[0]-1-offset)] end
+		content += url.expanded_url
+		laststart = url.indices[1]-offset
+	end
+	content += tweet[laststart..-1]
+	
+	# (consider logging id/text/timestamp)
+	
+	# Generate the QR 
+	size = 4
+	begin
+		qr = RQRCode::QRCode.new(content, :size => size)
+	rescue RQRCode::QRCodeRunTimeError => qerr
+
+		# Content too big for size. Let error stand if already largest size.
+		if size == 40 then raise qerr end
+		
+		# Otherwise, increase the size and try again.
+		size += 1
+		retry
+		
+	end
+
+	# Generate a PNG image of the QR code
+	png = qr.to_img.resize(480, 480)
+	
+	# Post the QR code image in reply; rgb format explicit (default grayscale PNG appears to confuse Twitter)
+	begin
+		reply = twitter.update_with_media(format("@%s ", mention.user.screen_name), png.to_datastream(:fast_rgb), :in_reply_to_status_id => mention.id)
+	rescue Twitter::Error => err
+		puts STDERR, err
+	end
+	
+end
+
+# Overwrite config file to update mostRecentMentionId.
+File.open('qr9000.yml', 'w') {|file| YAML.dump(config, file)}
